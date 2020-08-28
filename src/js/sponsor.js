aa66563e4ff2a9186c2022a47417567c3d7afd65
WuGenQiang <2422676183@qq.com>
2020-08-28 17:21:18 +0800

    Initial commit

---
+++ src/js/sponsor.js
@@ -0,0 +1,32 @@
+const bd = document.querySelector('body')
+const qbox = bd.querySelector('#QRBox')
+const mbox = bd.querySelector('#MainBox')
+const dbox = bd.querySelector('#donateBox')
+
+const qqr = 'https://wugenqiang.js.org/images/02/QQPayQR.png'
+const aqr = 'https://wugenqiang.js.org/images/02/AliPayQR.png'
+const wqr = 'https://wugenqiang.js.org/images/02/WeChanQR.png'
+
+const showQR = (QR) => {
+	if (QR) mbox.style.backgroundImage = `url(${ QR })`
+	bd.classList.add('blur')
+	qbox.classList.add('fadeIn')
+	mbox.classList.add('showQR')
+}
+
+dbox.addEventListener('click', (e) => {
+	let el = e.target
+	if (el.id === 'QQPay') showQR(qqr)
+	else if (el.id === 'AliPay') showQR(aqr)
+	else if (el.id === 'WeChat') showQR(wqr)
+})
+
+mbox.addEventListener('click', () => {
+	mbox.classList.remove('showQR')
+	mbox.classList.add('hideQR')
+	setTimeout(a => {
+		qbox.classList.remove('fadeIn')
+		mbox.classList.remove('hideQR')
+		bd.classList.remove('blur')
+	}, 600)
+})
